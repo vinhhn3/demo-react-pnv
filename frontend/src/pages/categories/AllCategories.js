b2eb9d411751d77d2181880fa6bff94b49161f27
Vinh Hoang <vinhhn3@fpt.edu.vn>
2024-05-24 16:06:04 +0700

    fix: Update AllCategories component to use react-router-dom for navigation

--- frontend/src/pages/categories/AllCategories.js
+++ frontend/src/pages/categories/AllCategories.js
@@ -1,19 +1,24 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const AllCategories = () => {
   const [categories, setCategories] = useState([]);
-
+  const navigate = useHistory();
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(async () => {
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  const getCategories = async () => {
     const res = await axios.get("http://localhost:3000/category");
     console.log(res);
     setCategories(res.data);
-  }, []);
+  };
   return (
     <div>
       <h1>All Categories</h1>
-      <a href="/categories/add">Add Category</a>
+      <Link to="/categories/add">Add Category</Link>
     </div>
   );
 };
